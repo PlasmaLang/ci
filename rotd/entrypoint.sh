e48d82aab9f307918bac2d1da8da39ce428a91ab
Paul Bone <paul@bone.id.au>
2020-02-27 00:16:59 +1100

    Add a buildType input

--- rotd/entrypoint.sh
+++ rotd/entrypoint.sh
@@ -19,9 +19,28 @@ case "$INPUT_C" in
         ;;
 esac
 
+BUILD_TYPE=$INPUT_BUILDTYPE
+case "$INPUT_BUILDTYPE" in
+    dev)
+        C_CXX_FLAGS='$(C_CXX_FLAGS_BASE) -O1 -DDEBUG -DPZ_DEV';
+        MCFLAGS="-O1";
+        break;
+        ;;
+    rel)
+        C_CXX_FLAGS='$(C_CXX_FLAGS_BASE) -O3';
+        MCFLAGS="-O4 --intermodule-optimisation";
+        break;
+        ;;
+    *)
+        echo "Unknown build type $INPUT_BUILDTYPE";
+        exit 1;
+        ;;
+esac
+
 echo C compiler
 echo ----------
 $CC --version
+echo C_CXX_FLAGS is $C_CXX_FLAGS
 echo
 
 echo Mercury compiler
@@ -34,6 +53,9 @@ cat > build.mk << END
 JOBS=2
 CC=$CC
 CXX=$CXX
+C_CXX_FLAGS=$C_CXX_FLAGS
+MCFLAGS=$MCFLAGS
+BUILD_TYPE=$BUILD_TYPE
 END
 
 echo build.mk:
