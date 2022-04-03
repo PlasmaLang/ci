638d44bb9f32b695ff1bdfcbae5e4a8c8de03e3f
Paul Bone <paul@bone.id.au>
2022-04-03 14:18:08 +1000

    Bump version for `plasma-ci-dep` and install lua

--- plasma-ci-dep/rotd/build.sh
+++ plasma-ci-dep/rotd/build.sh
@@ -6,10 +6,10 @@ cp ../install.sh .
 
 docker build \
     -t paulbone/plasma-ci-dep-rotd:v1 \
-    -t paulbone/plasma-ci-dep-rotd:v1_1 \
+    -t paulbone/plasma-ci-dep-rotd:v1_2 \
     -t paulbone/plasma-ci-dep-rotd:latest \
     .
 docker push paulbone/plasma-ci-dep-rotd:v1
-docker push paulbone/plasma-ci-dep-rotd:v1_1
+docker push paulbone/plasma-ci-dep-rotd:v1_2
 docker push paulbone/plasma-ci-dep-rotd:latest
 
