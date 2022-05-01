02f8c7c672608fb6061a319c216aee33cfaaee57
Paul Bone <paul@bone.id.au>
2022-05-01 15:12:08 +1000

    Update the Plasma CI to use Mercury 22.01

--- plasma-ci-dep/stable/build.sh
+++ plasma-ci-dep/stable/build.sh
@@ -4,6 +4,12 @@ set -e
 
 cp ../install.sh .
 
-docker build -t paulbone/plasma-ci-dep:stable .
-docker push paulbone/plasma-ci-dep:stable
+docker build \
+    -t paulbone/plasma-ci-dep-stable:latest \
+    -t paulbone/plasma-ci-dep-stable:v1 \
+    -t paulbone/plasma-ci-dep-stable:v1_0 \
+    .
+docker push paulbone/plasma-ci-dep-stable:latest
+docker push paulbone/plasma-ci-dep-stable:v1
+docker push paulbone/plasma-ci-dep-stable:v1_0
 
